d99a2fa0accd632ff2918c5b4fa8f9582ed34224
Thrashattack <henrique2092@gmail.com>
2025-03-14 19:05:24 +0100

    fix: package.swift target paths

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -10,22 +9,33 @@ let package = Package(
         .iOS(.v13)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "FlizpaySDK",
-            targets: ["FlizpaySDK"]),
+            targets: ["FlizpaySDK"]
+        ),
+        .library(
+            name: "Lib",
+            targets: ["Lib"]
+        )
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        //.package(url: /* package url */, from: "1.0.0"),
+        // Add dependencies if needed
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "FlizpaySDK"),
+            name: "FlizpaySDK",
+            dependencies: [],
+            path: "Sources/FlizpaySDK"
+        ),
+        .target(
+            name: "Lib",
+            dependencies: [],
+            path: "Sources/Lib"
+        ),
         .testTarget(
             name: "FlizpaySDKTests",
-            dependencies: ["FlizpaySDK"]),
+            dependencies: ["FlizpaySDK"],
+            path: "Tests/FlizpaySDKTests"
+        )
     ]
 )
