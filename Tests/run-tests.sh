154c71607e5b5fa3e14c716760af7df4b3505b4f
Thrashattack <henrique2092@gmail.com>
2025-03-15 02:25:43 +0100

    fix: coverage report and coverage signature

--- Tests/run-tests.sh
+++ Tests/run-tests.sh
@@ -5,4 +5,4 @@ xcodebuild test \
   -destination 'platform=iOS Simulator,name=iPhone 15,OS=latest' \
   -sdk iphonesimulator \
   -enableCodeCoverage YES \
-  CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
+  CODE_SIGN_IDENTITY="FLIZpay" CODE_SIGNING_REQUIRED=NO
