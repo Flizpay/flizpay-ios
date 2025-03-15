f73d07aa4fe0de82590458659a770aa0dced326d
Thrashattack <henrique2092@gmail.com>
2025-03-15 06:47:44 +0100

    fix: force lcov format

--- Scripts/generate-coverage.sh
+++ Scripts/generate-coverage.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-OUTPUT_FILE="coverage/coverage.json"
+OUTPUT_FILE="coverage/lcov.info"
 IGNORE_FILENAME_REGEX=".build|Tests|Pods|Carthage|DerivedData"
 BUILD_PATH="DerivedData"
 
@@ -8,7 +8,7 @@ INSTR_PROFILE=$(find $BUILD_PATH -name "*.profdata")
 mkdir -p $(dirname "$OUTPUT_FILE")
 
 # Export to code coverage file
-xcrun llvm-cov export \
+xcrun llvm-cov export -format=lcov \
   "DerivedData/Build/Products/Debug-iphonesimulator/FlizpaySDK.o" \
   --instr-profile=$INSTR_PROFILE \
   --ignore-filename-regex=$IGNORE_FILENAME_REGEX > $OUTPUT_FILE
