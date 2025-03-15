9c67da4c5859e8688b41e492a8c587851bc6d2db
Thrashattack <henrique2092@gmail.com>
2025-03-15 05:19:51 +0100

    fix: coverage scripts

---
+++ Scripts/generate-coverage.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+OUTPUT_FILE="coverage/lcov.info"
+IGNORE_FILENAME_REGEX=".build|Tests|Pods|Carthage|DerivedData"
+BUILD_PATH="DerivedData"
+
+INSTR_PROFILE=$(find $BUILD_PATH -name "*.profdata")
+
+mkdir -p $(dirname "$OUTPUT_FILE")
+
+# Export to code coverage file
+xcrun llvm-cov export \
+  "DerivedData/Build/Products/Debug-iphonesimulator/FlizpaySDK.o" \
+  --instr-profile=$INSTR_PROFILE \
+  --ignore-filename-regex=$IGNORE_FILENAME_REGEX > $OUTPUT_FILE
--- Scripts/run-tests.sh
+++ Scripts/run-tests.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-
+DERIVED_DATA_PATH=./DerivedData
 xcodebuild test \
   -scheme FlizpaySDK \
   -destination 'platform=iOS Simulator,name=iPhone 15,OS=latest' \
-  -sdk iphonesimulator \
   -enableCodeCoverage YES \
-  CODE_SIGN_IDENTITY="FLIZpay" CODE_SIGNING_REQUIRED=NO
+  -derivedDataPath $DERIVED_DATA_PATH
