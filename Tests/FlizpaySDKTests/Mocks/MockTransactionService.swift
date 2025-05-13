436a13a5772bce3665199790a7ae47b00b1ddbb9
OlegSyng <olegsingaevskii9@gmail.com>
2025-05-13 19:37:29 +0300

    chore: fix tests

--- Tests/FlizpaySDKTests/Mocks/MockTransactionService.swift
+++ Tests/FlizpaySDKTests/Mocks/MockTransactionService.swift
@@ -4,7 +4,7 @@ import Foundation
 class MockTransactionService: TransactionService {
     var mockResult: Result<TransactionResponse, Error>?
     
-    override func fetchTransactionInfo(token: String, amount: String, completion: @escaping (Result<TransactionResponse, Error>) -> Void) {
+    override func fetchTransactionInfo(token: String, amount: String,  metadata: [String: JSONValue]? = nil, completion: @escaping (Result<TransactionResponse, Error>) -> Void) {
         if let result = mockResult {
             completion(result)
         }
