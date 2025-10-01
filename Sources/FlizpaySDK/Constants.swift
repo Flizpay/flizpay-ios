0b5b89dedf0e8759d8343cde8964276267a9be27
felixswift <felixmpaulus@googlemail.com>
2025-10-01 17:36:23 +0200

    chore: update API and base URLs to use Tailscale domains

--- Sources/FlizpaySDK/Constants.swift
+++ Sources/FlizpaySDK/Constants.swift
@@ -1,4 +1,9 @@
 public class Constants {
-    public static let apiURL: String = "https://api-staging.flizpay.de"
-    public static let baseURL: String = "https://secure.flizpay.de"
+    // make sure to add your tailscale domain here
+    // public static let apiURL: String = "https://api-staging.flizpay.de"
+    public static let apiURL: String = "https://felixs-macbook-pro.tail0c1e83.ts.net:4440"
+    
+    // make sure to add your tailscale domain here
+    // public static let baseURL: String = "https://secure.flizpay.de"
+    public static let baseURL: String = "https://felixs-macbook-pro.tail0c1e83.ts.net:4442"
 }
