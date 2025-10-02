89db0190ab91d1d6049d3952d278ff1cdbffc442
felixfliz <felix.paulus@flizpay.de>
2025-10-02 09:30:18 +0200

    Coderabbit update Sources/FlizpaySDK/Constants.swift
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- Sources/FlizpaySDK/Constants.swift
+++ Sources/FlizpaySDK/Constants.swift
@@ -1,9 +1,5 @@
 public class Constants {
-    // make sure to add your tailscale domain here
-    // public static let apiURL: String = "https://api-staging.flizpay.de"
-    public static let apiURL: String = "https://felixs-macbook-pro.tail0c1e83.ts.net:4440"
-    
-    // make sure to add your tailscale domain here
-    // public static let baseURL: String = "https://secure.flizpay.de"
-    public static let baseURL: String = "https://felixs-macbook-pro.tail0c1e83.ts.net:4442"
+    public static let apiURL: String = "https://api.flizpay.de"
+
+    public static let baseURL: String = "https://secure.flizpay.de"
 }
