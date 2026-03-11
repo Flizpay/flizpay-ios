b99dfc3d5bd499ba541e6aa882b87e79d46012ca
OlegSyng <olegsingaevskii9@gmail.com>
2026-03-11 16:56:58 +0200

    chore: update destination to available in runner

--- Scripts/run-tests.sh
+++ Scripts/run-tests.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+
 DERIVED_DATA_PATH=DerivedData
+DESTINATION="${IOS_TEST_DESTINATION:-platform=iOS Simulator,name=iPhone 16,OS=latest}"
+
 xcodebuild test \
   -scheme FlizpaySDK \
-  -destination 'platform=iOS Simulator,name=iPhone 15,OS=latest' \
+  -destination "$DESTINATION" \
   -enableCodeCoverage YES \
   -derivedDataPath $DERIVED_DATA_PATH
